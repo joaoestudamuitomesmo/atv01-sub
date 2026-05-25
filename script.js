e5fb5b490bf756e78c661b51f6cdc05050eccd25
João Victor Marques da Silva <jmarquessva61@gmail.com>
2026-05-25 08:07:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function clicar(onde){
+    switch(onde){
+        case 1:
+            window.open("http://www.x.com", "_blank");
+            break;
+        case 2:
+            window.open("http://www.instagram.com", "_blank");
+            break;
+        case 3:
+            window.open("http://www.youtube.com", "_blank");
+            break;
+    }
+}
